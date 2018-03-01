fbc5bb0e2af7d1f60232db85da11615708a36ea1
khiarkhiar23 <kkhiar@mail.csuchico.edu>
2018-02-28 16:13:39 -0800

    changes the greeting

--- hello.cpp
+++ hello.cpp
@@ -1,9 +1,9 @@
-#include <iostream>
+#include <iostream> // This is single-line comment because it follow the two foward-slash. 
 #include <string>
 
 int main() {
     std::string input;
-    std::cout << "What's your first name?" << std::endl;
+    std::cout << "What's your first name?" << std::endl; //instert name: khiar M khiar 
     std::cin >> input;
     std::cout << "Hello, " << input << std::endl;
     return 0;
